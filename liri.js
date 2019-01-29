0cd7ac5ffc6dfe55e38b7e79d52f871ca0e110a2
Noah A <nanderson815@gmail.com>
2019-01-29 12:53:16 -0500

    added keys

--- liri.js
+++ liri.js
@@ -7,5 +7,12 @@ var axios = require('axios');
 
 var Spotify = require('node-spotify-api');
 
+var keys = require("./keys");
+
 var spotify = new Spotify(keys.spotify);
+
+var omdb = keys.omdb;
+
+console.log(spotify);
+console.log(omdb);
 
