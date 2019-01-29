23ffea2c8691acfc1c43145f64d7b18979e74845
Noah A <nanderson815@gmail.com>
2019-01-29 12:38:59 -0500

    added required npm

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
 
+var moment = require('moment');
+moment().format();
+
+var axios = require('axios');
+
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
 
