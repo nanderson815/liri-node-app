6c546844c3bdacccaf0fd13470c166250ab27584
Noah A <nanderson815@gmail.com>
2019-01-29 17:15:19 -0500

    got bands in town working

--- liri.js
+++ liri.js
@@ -31,7 +31,13 @@ if (command === "concert-this") {
         var URL = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=" + bandsKey;
         axios.get(URL)
             .then(function (resp) {
-                console.log(resp.data[0]);
+                for (i in resp.data) {
+                    num = parseInt(i) + 1;
+                    console.log("------------------ Concert " + num + " -------------------");
+                    console.log(resp.data[i].venue.name);
+                    console.log(resp.data[i].venue.city);
+                    console.log(moment(resp.data[i].datetime).format("MM/DD/YYYY"));
+                }
             })
     } else {
         console.log("Please enter a band name in quotes.");
